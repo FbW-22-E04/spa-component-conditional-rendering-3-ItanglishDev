6752b972b985a2eec46cf31085bba2dd66f3f09d
chris <chrishethertonweb@gmail.com>
2022-10-11 14:42:57 +0200

    solved

--- src/Alert.js
+++ src/Alert.js
@@ -1,12 +1,18 @@
 import React from "react"
+
+
 const Alert = props => {
 	//add the condition inside this function
 
-	return (
-		<div className="alert alert-danger" role="alert">
-			This is a primary alert-check it out!
-		</div>
-	);
+	const { text, show } = props
+
+	if ({ show })
+		return (
+			// <div className="alert alert-danger" role="alert">
+			// 	This is a primary alert-check it out!
+			// </div>
+			{ text }
+		);
 };
 
 export default Alert;
